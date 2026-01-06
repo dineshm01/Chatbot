b2750d2621abf0e70988acfb626dbb56f2633b27
dineshm01 <dineshmmp18@gmail.com>
2026-01-06 15:56:03 +0530

    Update ingest.py

--- backend/ingest.py
+++ backend/ingest.py
@@ -1,11 +1,8 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from utils.loaders import load_file
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+import os
 
 def ingest_document(file_path):
     docs = load_file(file_path)
@@ -14,8 +11,12 @@ def ingest_document(file_path):
         chunk_size=1000,
         chunk_overlap=150
     )
-
     chunks = splitter.split_documents(docs)
+
+    embeddings = HuggingFaceInferenceAPIEmbeddings(
+        api_key=os.getenv("HF_API_KEY"),
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
     vectorstore = FAISS.from_documents(chunks, embeddings)
     vectorstore.save_local("faiss_index")
