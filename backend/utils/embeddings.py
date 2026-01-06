c73a13ba707b3638332351e772e4b29415cc44a8
dineshm01 <dineshmmp18@gmail.com>
2026-01-06 15:34:49 +0530

    Update embeddings.py

--- backend/utils/embeddings.py
+++ backend/utils/embeddings.py
@@ -3,7 +3,6 @@ import os
 
 def get_embeddings():
     return HuggingFaceEndpointEmbeddings(
-        repo_id="sentence-transformers/all-MiniLM-L6-v2",
-        huggingfacehub_api_token=os.getenv("HF_API_KEY")
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        huggingfacehub_api_token=os.getenv("HF_API_KEY"),
     )
-
