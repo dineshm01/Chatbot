31874d0609a9aadfe23077f4c805560b803c9c0b
dineshm01 <dineshmmp18@gmail.com>
2026-01-06 17:46:32 +0530

    Update retriever.py

--- backend/utils/retriever.py
+++ backend/utils/retriever.py
@@ -1,7 +1,4 @@
 from langchain_community.vectorstores import FAISS
-from utils.embeddings import get_embeddings
 
-VECTOR_DIR = "vectorstore"
-
-def load_vectorstore():
-    return FAISS.load_local(VECTOR_DIR, get_embeddings(), allow_dangerous_deserialization=True)
+def create_vectorstore(docs, embeddings):
+    return FAISS.from_documents(docs, embeddings)
