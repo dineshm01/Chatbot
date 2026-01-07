191871b58f7a660c50478f3e6872c8cbd3fc3c4d
dineshm01 <dineshmmp18@gmail.com>
2026-01-07 13:24:24 +0530

    Update embeddings.py

--- backend/utils/embeddings.py
+++ backend/utils/embeddings.py
@@ -1,4 +1,12 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceInferenceAPIEmbeddings
+import os
 
 def get_embeddings():
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    token = os.getenv("HF_API_KEY")
+    if not token:
+        raise RuntimeError("HUGGINGFACEHUB_API_TOKEN is not set")
+
+    return HuggingFaceInferenceAPIEmbeddings(
+        api_key=token,
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
