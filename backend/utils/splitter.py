c00300a0061afdd45015e2bcab8f1fabf90b14c8
dineshm01 <dineshmmp18@gmail.com>
2026-01-07 11:01:55 +0530

    Delete backend/utils/splitter.py

--- backend/utils/splitter.py
+++
@@ -1,8 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def split_documents(documents):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=300
-    )
-    return splitter.split_documents(documents)
