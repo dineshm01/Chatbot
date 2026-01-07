4f0b0de7f75cb4d679f3caa6f07ded57d5a5ca46
dineshm01 <dineshmmp18@gmail.com>
2026-01-07 12:13:56 +0530

    Update ingest.py

--- backend/ingest.py
+++ backend/ingest.py
@@ -1,13 +1,9 @@
-import os
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import UnstructuredPowerPointLoader, PyMuPDFLoader
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
 from utils.embeddings import get_embeddings
-from utils.retriever import create_vectorstore
-
 
 VECTOR_DIR = "vectorstore"
-os.makedirs(VECTOR_DIR, exist_ok=True)
 
 def ingest_document(filepath):
     loader = UnstructuredPowerPointLoader(filepath)
@@ -20,10 +16,3 @@ def ingest_document(filepath):
 
     vectorstore = FAISS.from_documents(chunks, embeddings)
     vectorstore.save_local(VECTOR_DIR)
-
-    vectorstore.save_local(VECTOR_DIR)
-
-    print("INGEST: vectorstore saved")
-
-
-
