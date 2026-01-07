1b8b2fe530b16374fa8aac4360e4f1d77394caf1
dineshm01 <dineshmmp18@gmail.com>
2026-01-07 13:41:44 +0530

    Update embeddings.py

--- backend/utils/embeddings.py
+++ backend/utils/embeddings.py
@@ -1,8 +1,12 @@
-from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 import os
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 
 def get_embeddings():
+    HF_API_KEY = os.getenv("HF_API_KEY")
+    if not HF_API_KEY:
+        raise RuntimeError("HUGGINGFACE_API_KEY not set in environment")
+
     return HuggingFaceInferenceAPIEmbeddings(
-        HF_API_KEY=os.getenv("HF_API_KEY"),
+        HF_API_KEY=HF_API_KEY,
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
